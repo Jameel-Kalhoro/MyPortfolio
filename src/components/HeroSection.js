dd2cd4242c63bae835ce9f72e990d5cd903df9be
unknown <jameel.bscsf19@iba-suk.edu.pk>
2023-10-30 23:52:40 -0700

    new update

---
+++ src/components/HeroSection.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const HeroSection = () => (
+  <section>
+    
+  </section>
+);
+
+export default HeroSection;
---
+++ src/components/Reviews.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+export const BuyerReview = () => (
+  <div>
+    
+  </div>
+);
+
+export const SellerReview = () => (
+  <div>
+    
+  </div>
+);
